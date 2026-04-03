7a0342c7872e36f56dcad03f15513e0e47066384
Teddy Habib <gtedz1961@gmail.com>
2026-04-03 16:02:27 +0300

    Updated non-signing KeyPairs to throw errors
    KeyPairs that can't sign (NilKeyPair and ApiKeyOnlyKeyPair) now throw errors so that the Request aborts early instead of the HTTP Request going through and returning an error

--- internal/apikey/apikey_only.go
+++ internal/apikey/apikey_only.go
@@ -1,10 +1,12 @@
 package apikey
 
+import "fmt"
+
 type ApiKeyOnlyKeyPair struct {
 	apiKey string
 }
 
 func (keypair *ApiKeyOnlyKeyPair) ApiKey() string { return keypair.apiKey }
 func (keypair *ApiKeyOnlyKeyPair) Sign(value string) (string, error) {
-	return "", nil
+	return "", fmt.Errorf("no private key present to create a signature")
 }
