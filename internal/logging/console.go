5e8c55583ebc04aa96cb7156ba2589ca36435822
Teddy Habib <gtedz1961@gmail.com>
2026-06-06 01:08:59 +0300

    Removed coloring and shit

--- internal/logging/console.go
+++ internal/logging/console.go
@@ -2,19 +2,8 @@ package logging
 
 import (
 	"fmt"
-	"os"
-
-	"golang.org/x/sys/windows"
 )
 
-func init() {
-	stdout := windows.Handle(os.Stdout.Fd())
-	var originalMode uint32
-
-	windows.GetConsoleMode(stdout, &originalMode)
-	windows.SetConsoleMode(stdout, originalMode|0x0004)
-}
-
-func print(level LogLevel, message string) {
-	fmt.Print(level.Color() + message + "\x1b[0m" + "\n")
+func print(_ LogLevel, message string) {
+	fmt.Print(message)
 }
